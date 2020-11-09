d7fabef46041d6d3b84a25ac254b3d2465ff9484
Olivia Cubela <oliviacubela@gmail.com>
2020-11-09 16:01:58 -0500

    done.

---
+++ Hello.cs
@@ -0,0 +1,11 @@
+using System;
+// using System is called a directive... because C# is compiled, we need to tell it what code it needs in order to function.
+
+class Program
+{
+//  This is an entry point - methods that automatically run when a program is launched are called entry points, like static void Main()
+  static void Main()
+  {
+    Console.WriteLine("Hello World!");
+  }
+}
